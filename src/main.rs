e090c43939c1acfb1fb9ef249ed9479d6dd4a62d
Lotaanywaki Mollel <loymollel@habari.co.tz>
2023-02-18 17:22:43 +0300

    Configuring HTTP server and logging

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 extern crate dotenv;
+extern crate log;
 
-use actix_web::{ middleware, web, App, HttpServer };
+use actix_web::{ web, App, HttpServer };
 use sqlx::mysql::MySqlPoolOptions;
 
 use dotenv::dotenv;
 use std::{env};
+use log::{info};
 
 #[actix_web::main]
-async fn main() -> Result<(), sqlx::Error> {
-
+async fn main() -> std::io::Result<()> {
     dotenv().expect("Failed to read .env file");
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("error"));
+
+    info!("Starting up");
+
+    let app_host = env::var("APP_HOST").expect("APP_HOST not found.");
+    let app_port = env::var("APP_PORT").expect("APP_PORT not found.");
+    let app_url = format!("{}:{}", &app_host, &app_port);
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL not found in .env file.");
 
     let pool = MySqlPoolOptions::new()
@@ -18,13 +26,14 @@ async fn main() -> Result<(), sqlx::Error> {
         .await
         .expect("Unable to connect to MySQL Database");
 
-    let email = "admin@email.com";
-    let row: (i64,) = sqlx::query_as("SELECT * FROM users WHERE email = ?")
-        .bind(email)
-        .fetch_one(&pool)
-        .await?;
+    info!("Listening on: {}", &app_url);
 
-    assert_eq!(row.0, 0);
-
-    Ok(())
+    // Start HTTP Server
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+    })
+        .bind(&app_url)?
+        .run()
+        .await
 }
